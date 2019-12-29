2ebbdb5bab6af587f4569d39223e64c8596a1283
r.golusinski <r.golusinski@samsung.com>
2019-12-29 11:43:56 +0100

    create navigation and implement navigation from the title screen to game screen

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -19,6 +20,9 @@ class TitleFragment : Fragment() {
                 R.layout.fragment_title,
                 container,
                 false
+        )
+        binding.playButton.setOnClickListener(
+                Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
         )
         return binding.root
     }
